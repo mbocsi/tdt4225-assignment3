c1ee7becae3252cca7be7cd5002f31cbab78b6b2
Marton Bocsi <mbocsi@wisc.edu>
2023-10-26 14:15:16 +0200

    Implemented datastructures for database schema

--- Database.py
+++ Database.py
@@ -1,6 +1,88 @@
 from DbConnector import DbConnector
-from typing import Union
+from typing import Union, Any
 import logging
+
+class User(dict):
+    KEYS = ('id', 'has_labels', 'activites')
+    def __init__(self,
+                 id : str,
+                 has_labels : bool,
+                 activites : list = []) -> None:
+        self._dictionary = {'id': id,
+                            'has_labels': has_labels,
+                            'activities': activites}
+        
+    def __setitem__(self, key : str, item : Any) -> None:
+        if key not in User.KEYS:
+            raise KeyError(f"The key {key} is not defined.")
+        self._dictionary[key] = item
+
+    def __getitem__(self, key : str) -> Any:
+        return self._dictionary[key]
+
+class ActivityDenorm(dict):
+    KEYS = ('id', 'user')
+    def __setitem__(self, key : str, item : Any) -> None:
+        if key not in User.KEYS:
+            raise KeyError(f"The key {key} is not defined.")
+        self._dictionary[key] = item
+
+    def __getitem__(self, key : str) -> Any:
+        return self._dictionary[key]
+
+class Activity(dict):
+    KEYS = ('id', 'user', 'transportation_mode', 'start_date_time', 'end_date_time', 'trackpoints')
+    DENORM_KEYS = ('id', 'user')
+    def __init__(self,
+                 id : int,
+                 user : str,
+                 transportation_mode : str = None,
+                 start_date_time : str = None,
+                 end_date_time : str = None,
+                 trackpoints : list = []) -> None:
+        self._dictionary = {'id': id,
+                            'user': user,
+                            'transportation_mode': transportation_mode,
+                            'start_date_time': start_date_time,
+                            'end_date_time': end_date_time,
+                            'trackpoints': trackpoints}
+        
+    def __setitem__(self, key : str, item : Any) -> None:
+        if key not in User.KEYS:
+            raise KeyError(f"The key {key} is not defined.")
+        self._dictionary[key] = item
+
+    def __getitem__(self, key : str) -> Any:
+        return self._dictionary[key]
+    
+    def denorm(self) -> ActivityDenorm:
+        return {key : self._dictionary[key] for key in Activity.DENORM_KEYS}
+
+class TrackPoint(dict):
+    KEYS = ('id', 'lat', 'lon', 'altitude', 'date_days', 'date_time', 'activity')
+    def __init__(self,
+                 id : int,
+                 lat : float,
+                 lon : float,
+                 altitude : int,
+                 date_days : float,
+                 date_time : str,
+                 activity : ActivityDenorm) -> None:
+        self._dictionary = {'id': id,
+                            'lat': lat,
+                            'lon': lon,
+                            'altitude': altitude,
+                            'date_days': date_days,
+                            'date_time': date_time,
+                            'activity': activity}
+        
+    def __setitem__(self, key : str, item : Any) -> None:
+        if key not in User.KEYS:
+            raise KeyError(f"The key {key} is not defined.")
+        self._dictionary[key] = item
+
+    def __getitem__(self, key : str) -> Any:
+        return self._dictionary[key]
 
 class Database:
     def __init__(self) -> None:
@@ -17,7 +99,7 @@ class Database:
         logging.info(f'Created a new collection: {collection}')
         return True
     
-    def insert_documents(self, collection : str, data : Union[dict, list[dict]]) -> bool:
+    def insert_documents(self, collection : str, data : Union[Union[Activity, User, TrackPoint], list[Union[Activity, User, TrackPoint]]]) -> bool:
         try:
             collection = self.db[collection]
             if isinstance(data, dict):
@@ -29,6 +111,15 @@ class Database:
             return False
         logging.info(f'Inserted data into: {collection}')
         return True
+    
+    def insert_user(self, user : Union[User, list[User]]) -> bool:
+        return self.insert_documents('User', user)
+    
+    def insert_activities(self, activities : Union[Activity, list[Activity]]) -> bool:
+        return self.insert_documents('Activity', activities)
+    
+    def insert_trackpoints(self, trackpoints : Union[TrackPoint, list[TrackPoint]]) -> bool:
+        return self.insert_documents('TrackPoint', trackpoints)
     
     def drop_collection(self, collection : str) -> bool:
         try:
--- part1.py
+++ part1.py
@@ -1,4 +1,107 @@
-from Database import Database
+from Database import Database, User, Activity, TrackPoint
+from pathlib import Path
+import os
+from datetime import datetime
+import logging
+from icecream import ic
 
-if __name__ == '__main__':
+FORMAT = '%(asctime)s : %(message)s'
+logging.basicConfig(filename='part1.log', filemode='w', level=logging.INFO, format=FORMAT)
+
+def main() -> None:
+    global activity_counter
+    activity_counter = 0
+    LABELED_IDS = []
+    with open(Path('.') / 'dataset' / 'labeled_ids.txt', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            LABELED_IDS.append(line.replace('\n', ''))
+    logging.debug(LABELED_IDS)
+
+    # Create the collections
     db = Database()
+    if not db.create_collection('User'):
+        quit()
+    if not db.create_collection("Activity"):
+        quit()
+    if not db.create_collection("TrackPoint"):
+        quit()
+
+    user_obj = None
+    activities = []
+    for root, dirs, files in os.walk("dataset/Data", topdown=True):
+        match len(Path(root).parts):
+            case 3: # User 
+                user = Path(root).parts[2]
+                user_obj = User(user, user in LABELED_IDS)
+            case 4: # Activity
+                activity = Activity(activity_counter, user_obj['id'])
+                trackpoints = []
+                for file in files:
+                    if file[-3:] != 'plt':
+                        continue
+
+                    filename = file.split('.')[0] # Activity ID
+                    plt = []
+
+                    with open(Path(root) / file, 'r') as f:
+                        plt = f.readlines()
+
+                    if len(plt[6:]) > 2500: # Only insert activites with fewer than 2501 points
+                        logging.info(f'Skipped activity: {filename}! TOO BIG!')
+                        continue
+
+                    activity_counter += 1
+                    activity = Activity(activity_counter, user)
+
+                    first_line = plt[6].split(',')
+                    start_date = first_line[-2]
+                    start_time = first_line[-1].replace('\n', '')
+                    start_datetime = datetime.strptime(f'{start_date} {start_time}', '%Y-%m-%d %H:%M:%S', )  # Start datetime
+
+                    last_line = plt[-1].split(',')
+                    end_date = last_line[-2]
+                    end_time = last_line[-1].replace('\n', '')
+                    end_datetime = datetime.strptime(f'{end_date} {end_time}', '%Y-%m-%d %H:%M:%S') # End datetime
+
+                    transportation = None # Transportation mode
+                    if user in LABELED_IDS:
+                        with open(Path(*Path(root).parts[:3]) / 'labels.txt', 'r') as f:
+                            labels = f.readlines()
+                        for label in labels[1:]:
+                            label = label.split()
+                            label_start = datetime.strptime(f'{label[0]} {label[1]}', '%Y/%m/%d %H:%M:%S')
+                            label_end = datetime.strptime(f'{label[2]} {label[3]}', '%Y/%m/%d %H:%M:%S', )
+                            if label_start == start_datetime and label_end == end_datetime:
+                                transportation = label[4]
+                                break
+                    
+                    logging.debug(f'Inserting: {(activity_counter, user, transportation, start_datetime, end_datetime)}')
+
+                    # Insert the activity into the list
+                    activity['transportation_mode'] = transportation
+                    activity['start_date_time'] = start_datetime.strftime('%Y-%m-%d %H:%M:%S')
+                    activity['end_date_time'] = end_datetime.strftime('%Y-%m-%d %H:%M:%S')
+                    activities.append(activity)
+                    logging.info(f'Inserted activity: {(activity_counter, user, transportation, start_datetime, end_datetime)}')
+
+                    track_points = []
+                    for point in plt[6:]:
+                        lat, lon, _, alt, days, date, time = point.split(',')
+                        time = time.replace('\n', '')
+                        point_datetime = f"{date} {time}"
+                        track_points.append((activity_counter, lat, lon, alt, days, point_datetime))
+
+                    # Insert the trackpoints associated with the activity
+                    if not db.insert_trackpoints(track_points):
+                        quit()
+                    logging.info(f'Inserted trackpoints for activity: {activity_counter}')
+                    
+if __name__ == '__main__':
+    # main()
+    for root, dirs, files in os.walk("dataset/Data", topdown=True):
+        # for name in files:
+        #     print(os.path.join(root, name))
+        ic(root)
+        ic(dirs)
+        ic(files)
