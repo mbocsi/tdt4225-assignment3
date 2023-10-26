acecf38c69c772112607ed470e36a0eb9f3a5954
Marton Bocsi <mbocsi@wisc.edu>
2023-10-26 22:50:03 +0200

    Added better comments on part1.py

--- part1.py
+++ part1.py
@@ -4,16 +4,19 @@ from pathlib import Path
 import os
 from datetime import datetime
 import logging
-from icecream import ic
 
 FORMAT = '%(asctime)s : %(levelname)s : %(message)s'
 logging.basicConfig(filename='part1.log', filemode='w', level=logging.INFO, format=FORMAT)
 
 def main() -> None:
+    """
+    Main script for inserting all the data into the database
+    """
     global activity_counter
     global trackpoint_counter
     trackpoint_counter = 0
     activity_counter = 0
+
     LABELED_IDS = []
     with open(Path('.') / 'dataset' / 'labeled_ids.txt', 'r') as f:
         lines = f.readlines()
@@ -53,18 +56,18 @@ def main() -> None:
                         logging.debug(f'Skipped activity: {filename}! TOO BIG! (size={len(plt[6:])})')
                         continue
 
-                    activity = Activity(activity_counter, user, trackpoints=[])
+                    activity = Activity(activity_counter, user, trackpoints=[])                                 # Create the Activity document
                     activity_counter += 1
 
                     first_line = plt[6].split(',')
                     start_date = first_line[-2]
                     start_time = first_line[-1].replace('\n', '')
-                    start_datetime = datetime.strptime(f'{start_date} {start_time}', '%Y-%m-%d %H:%M:%S', )  # Start datetime
+                    start_datetime = datetime.strptime(f'{start_date} {start_time}', '%Y-%m-%d %H:%M:%S', )     # Start datetime
 
                     last_line = plt[-1].split(',')
                     end_date = last_line[-2]
                     end_time = last_line[-1].replace('\n', '')
-                    end_datetime = datetime.strptime(f'{end_date} {end_time}', '%Y-%m-%d %H:%M:%S') # End datetime
+                    end_datetime = datetime.strptime(f'{end_date} {end_time}', '%Y-%m-%d %H:%M:%S')             # End datetime
 
                     transportation = None # Transportation mode
                     if user in LABELED_IDS:
@@ -77,11 +80,11 @@ def main() -> None:
                             if label_start == start_datetime and label_end == end_datetime:
                                 transportation = label[4]
                                 break
-
-                    # Insert the activity into the list
+                    
+                    # Update the Activity document
                     activity['transportation_mode'] = transportation
-                    activity['start_date_time'] = start_datetime # .strftime('%Y-%m-%d %H:%M:%S')
-                    activity['end_date_time'] = end_datetime # .strftime('%Y-%m-%d %H:%M:%S')
+                    activity['start_date_time'] = start_datetime
+                    activity['end_date_time'] = end_datetime
 
                     for point in plt[6:]:
                         lat, lon, _, alt, days, date, time = point.split(',')
@@ -93,20 +96,25 @@ def main() -> None:
                                                 altitude=alt,
                                                 date_days=days,
                                                 date_time=point_datetime,
-                                                activity=activity.denorm())
-                        activity['trackpoints'].append(trackpoint['_id'])
-                        track_points.append(trackpoint)
+                                                activity=activity.denorm()) # Create TrackPoint document
+                        activity['trackpoints'].append(trackpoint['_id'])   # Update the Activity document with trackpoints
+                        track_points.append(trackpoint)                     # Insert trackpoint into list of trackpoints for insertion later
                         logging.debug(f"Created TrackPoint: {trackpoint}")
                         trackpoint_counter += 1
-                    activities.append(activity)
+                    activities.append(activity)                             # Insert activity into list of activites for insertion later
                     user_obj['activities'].append(activity['_id'])
                     logging.debug(f"Created Activity: {activity}")
+
+                # Insert all data associated with the current user into the database
                 db.insert_trackpoints(track_points)
                 db.insert_activities(activities)
                 db.insert_user(user_obj)
                 logging.info(f"Created User: {user_obj}")
                     
 def dropall() -> None:
+    """
+    Script for resetting the database (Used during development)
+    """
     db = Database()
     db.drop_collection("User")
     db.drop_collection("Activity")
